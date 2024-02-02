ea0570080362e8cb66d7d6a987388c840ef31c99
densogiaichned <dsian@posteo.de>
2024-02-02 20:01:30 +0100

    chore: enable file scoped namespace

--- tests/TcHaxx.Snappy.Tests/UnitTest1.cs
+++ tests/TcHaxx.Snappy.Tests/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace TcHaxx.Snappy.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace TcHaxx.Snappy.Tests;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
     }
 }
