3ea03110f5c8f28820885e587e8014980b956bad
densogiaichned <dsian@posteo.de>
2024-02-09 17:48:07 +0100

    chore(CLI/VerificationResultExtension): Add Regex for new files

--- src/TcHaxx.Snappy.Verifier/VerificationResultExtension.cs
+++ src/TcHaxx.Snappy.Verifier/VerificationResultExtension.cs
@@ -7,14 +7,19 @@ namespace TcHaxx.Snappy.Verifier
     {
         internal static VerificationResult ToCompactDiff(this VerificationResult result)
         {
-
-            var match = Regex.Match(result.Diff, "^Compare Result:\n(?<diff>.*)(?<!\n)$", RegexOptions.Singleline | RegexOptions.Multiline | RegexOptions.CultureInvariant | RegexOptions.Compiled);
-            if (!match.Success)
+            var matchNew = Regex.Match(result.Diff, "^New:\n\nReceived:(?<diff>.*)(?<!\n)$", RegexOptions.Singleline | RegexOptions.Multiline | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            if (matchNew.Success)
             {
+                result.Diff = $"New: {matchNew.Groups["diff"].Value}";
+                return result;
+            }
+            var matchCompareResult = Regex.Match(result.Diff, "^Compare Result:\n(?<diff>.*)(?<!\n)$", RegexOptions.Singleline | RegexOptions.Multiline | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            if (matchCompareResult.Success)
+            {
+                result.Diff = $"Compare Result:\n{matchCompareResult.Groups["diff"].Value}";
                 return result;
             }
 
-            result.Diff = match.Groups["diff"].Value;
             return result;
         }
     }
